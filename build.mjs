4116c22aef872baa73199eaf6bb9c7e45afeaae6
Pooya Parsa <pooya@pi0.io>
2025-03-06 23:01:02 +0100

    initial commit

---
+++ build.mjs
@@ -0,0 +1,33 @@
+import { build } from "esbuild";
+import { cp } from "node:fs/promises";
+import { readPackageJSON } from "pkg-types";
+
+const ver = (id) => readPackageJSON(id).then((m) => m.version);
+
+await build({
+  stdin: {
+    contents: /* js */ `export { Youch } from "youch"; export { ErrorParser } from "youch-core";`,
+    resolveDir: process.cwd(),
+  },
+  banner: {
+    js: [
+      "Copyright (c) virk.officials@gmail.com",
+      `Bundled https://github.com/poppinss/youch ${await ver("youch")} (MIT)`,
+      `Bundled https://github.com/poppinss/youch-core ${await ver("youch-core")} (MIT)`,
+    ]
+      .map((line) => `// ${line}`)
+      .join("\n"),
+  },
+  bundle: true,
+  outfile: "dist/youch.mjs",
+  platform: "node",
+  target: "esnext",
+  format: "esm",
+  legalComments: "inline",
+});
+
+const youchDir = new URL("public", import.meta.resolve("youch"));
+
+await cp(youchDir, "dist/public", {
+  recursive: true,
+});
